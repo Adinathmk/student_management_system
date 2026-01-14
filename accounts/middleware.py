13409233a589964f6d86af5e422c39646f7fa47b
Adinathmk <adinathmkclt@gmail.com>
2026-01-14 19:56:53 +0530

    middleware

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied
 
+
+
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.urls import reverse
+
+
 class AdminOnlyMiddleware:
     """
     Allows access to admin-panel URLs only for admin users.
@@ -22,3 +29,25 @@ class AdminOnlyMiddleware:
                 raise PermissionDenied
 
         return self.get_response(request)
+    
+
+
+
+class IsActiveMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user
+
+        # Check only if user is logged in
+        if user.is_authenticated and not user.is_active:
+            logout(request)
+            messages.error(
+                request,
+                "Your account is inactive. Please contact the administrator."
+            )
+            return redirect(reverse("login"))  # change if login url name differs
+
+        response = self.get_response(request)
+        return response
